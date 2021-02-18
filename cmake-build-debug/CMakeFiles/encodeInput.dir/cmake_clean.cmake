5135535ee4bee6b7398efe1cf7e3e574cfbda4f6
Filipe Brito <fbrito3808@conestogac.on.ca>
2021-02-17 22:06:16 -0800

    Added necessary files for the CLion IDE to open as a project.

---
+++ cmake-build-debug/CMakeFiles/encodeInput.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/encodeInput.dir/main.c.o"
+  "encodeInput"
+  "encodeInput.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/encodeInput.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
